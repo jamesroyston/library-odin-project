34569e8a93c34d63b697df0306e2c7d424bd2e21
jamesroyston <jamesroyston03@gmail.com>
2018-12-31 12:32:23 -0700

    feature: when removing or adding books, updates array first, then re-renders view

--- app.js
+++ app.js
@@ -1,69 +1,102 @@
-const app = document.querySelector('#app')
-
-const addBook = document.querySelector('#addBook')
-addBook.addEventListener('click', addBookToLib)
-
-function render (book) {
-  
-  const bookDiv = document.createElement('div')
-  bookDiv.classList.add('bookDiv')
-  bookDiv.innerHTML = 
-      `<h2>${book.name}</h2>
-      <p>Author: ${book.author}</p>
-      <p>Pages: ${book.pages}</p>
-      <p>Completed: ${book.read}
-          <span>
-              <button id="readToggle">toggle</button>
-          </span>
-      </p>
-      <button id="removeBook">Remove Book</button>`
-  app.appendChild(bookDiv)
-  document.querySelector('#removeBook')
-      .addEventListener('click', function() {
-        this.prototype.delete()
-        app.removeChild(bookDiv)
-      })
-}
+const table = document.querySelector('#main-table')
+const title = document.querySelector('#title_field')
+const author = document.querySelector('#author_field')
+const pages = document.querySelector('#pages_field')
+const read = document.querySelector('#read_field')
 
 let myLibrary = []
+let index = 0
 
-function displayLib() {
+const bookFields = [title, author, pages, read]
 
-  myLibrary.forEach(ball => {console.log(ball)})
+const render = book => {
+  const bookRow = document.createElement('tr')
+  bookRow.dataset.id = book.id
+  bookRow.innerHTML =
+    `<td class="table-item">
+            <button onclick="removeBook(this)">X</button>${book.name}
+        </td>
+        <td class="table-item">${book.author}</td>
+        <td class="table-item">${book.pages}</td>
+        <td class="table-item">
+            <button>read</button>
+            <button>not read</button>${book.read}
+        </td>`
+  table.appendChild(bookRow)
 }
 
-function Book (name, author, pages, read) {
+
+function addBookToLib(book) {
+  myLibrary.push(book)
+  table.innerHTML = ''
+  myLibrary.forEach(book => {
+    render(book)
+  })
+  for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary[i].id = i
+  }
+}
+
+/* Cody Loyd's advice:
+  instead of trying to surgically remove each book from the HTML,
+  remove the book from the array and re-render the view
+
+  mind blown
+*/
+
+function removeBook(x) {
+  let bookID = Number(x.closest('tr').dataset.id)
+  let libraryID = myLibrary[bookID].id
+  if (bookID === libraryID) {
+    console.log(`removed ${myLibrary[bookID].name}`)
+    myLibrary.splice(bookID, 1)
+    table.innerHTML = ''
+
+
+
+    for (let i = 0; i < myLibrary.length; i++) {
+      myLibrary[i].id = i
+    }
+    myLibrary.forEach(book => {
+      render(book)
+    })
+  }
+
+
+  console.table(myLibrary)
+}
+
+
+bookFields.forEach(prop => {
+  prop.addEventListener('keypress', e => {
+    if (e.keyCode === 13 || e.which === 13) {
+      addBookToLib(new Book(title.value, author.value, pages.value, read.value))
+      bookFields.forEach(value => value.value = '')
+      title.focus()
+      console.table(myLibrary)
+    }
+  })
+})
+
+function Book(name, author, pages, read) {
+  this.id = index++
   this.name = name
   this.author = author
   this.pages = pages
   this.read = read
 }
 
-Book.prototype.info = function () {
-  if (this.read == true) {
-    return `${this.name} by ${this.author}, ${this.pages} pages, finished`
-  } else {
-    return `${this.name} by ${this.author}, ${this.pages} pages, not done yet`
-    }
-}
 
-Book.prototype.delete = function () {
- for (let i = 0; i < myLibrary.length; i++) {
-   delete myLibrary[i]
- }
-}
-
-function addBookToLib (book) {
-
-  myLibrary.push(book)
-  render(book)
-}
-
-const book1 = new Book("48 Laws of Power", 'Author Man', 584, false)
-const book2 = new Book('Eloquent JavaScript', 'Another Author', 448, true)
-const book3 = new Book('Eloquent JavaScript', 'Another Author', 448, true)
-
-addBookToLib(book1)
-addBookToLib(book2)
-addBookToLib(book3)
-displayLib()
+const ex1 = new Book('99 Bottles Of OOP', "Sandi Metz", 456, false)
+const ex2 = new Book('Clean Code', "A Person", 456, true)
+const ex3 = new Book('Eloquent JavaScript', "A Person", 411, false)
+const ex4 = new Book('Green Eggs & Ham', "Dr. Suess", 45, true)
+const ex5 = new Book('H.P./Sorceror\'s Stone', "J.K. Rowlings",
+  600, true)
+addBookToLib(ex1)
+addBookToLib(ex2)
+addBookToLib(ex3)
+addBookToLib(ex4)
+addBookToLib(ex5)
+// console.table(myLibrary)
+// console.table(idArray)
